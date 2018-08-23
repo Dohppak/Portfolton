2b51592c890ca083b428e7e7137378ed2640ac6f
mac <seung4298@unist.ac.kr>
2018-08-23 19:14:09 +0900

    change

---
+++ js/sketch.js
@@ -0,0 +1,52 @@
+var total =50;
+var systems;
+var gravity;
+var repeller;
+var canvas;
+
+function setup() {
+    canvas = createCanvas(windowWidth,windowHeight);
+    canvas.position(0,0);
+    canvas.style('z-index','-1');
+    systems = [];
+    for(var i =0;i<total;i++){
+        systems.push(new Particle(createVector(floor(random(width)),floor(random(height)))));
+  }
+
+}
+
+function draw() {
+
+  background(51);
+  fill(255);
+    
+  textSize(20);
+  textFont("myFont");
+  text("Hello, I'm seungheon doh.", 50, 200);
+    
+
+  for(var i=0;i<systems.length;i++){
+    systems[i].show();
+
+    for(var j=0;j<systems.length;j++){
+      var distance = dist(systems[i].location.x,systems[i].location.y,systems[j].location.x,systems[j].location.y);
+      strokeWeight(0.5);
+
+      if(distance<200){
+        var lineAlpha = map(distance,0,200,255,0);
+        stroke(255,255,255,lineAlpha);
+        line(systems[i].location.x,systems[i].location.y,systems[j].location.x,systems[j].location.y);
+
+      }
+
+    }
+    systems[i].update();
+  }
+
+
+
+
+}
+function mousePressed(){
+systems.push(new Particle(createVector(mouseX,mouseY)));
+}
